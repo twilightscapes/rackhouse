5bea01512c1b1b38ce0785f26964f94b260a4fdc
twilightscapes <ihatespam@gmail.com>
2022-03-12 01:15:06 -0600

    Revert "Revert "ready1""
    This reverts commit f17595cca824b4fd5eb22f232cf1967fca4c6bdf.

--- src/components/install-discount.js
+++ src/components/install-discount.js
@@ -50,7 +50,7 @@ const InstallDiscount = () => (
 
 
 
-<div id="" className="" style={{ position:'absolute', top:'10vh', zIndex:'', maxWidth:'400px'}}>
+<div id="" className="" style={{ position:'', top:'', zIndex:'', maxWidth:''}}>
 
 
 {/* <ReactPlayer
@@ -129,7 +129,7 @@ const InstallDiscount = () => (
 
 
 
-<div className="container" style={{padding:'0 2rem', border:'0px solid #555',  color:'#fff', textAlign:'center', position:'relative', textShadow:'2px 2px 0 #000', overflow:'hidden', borderRadius:'12px', background:'rgb(0, 0, 0, .5)' }}>
+<div className="" style={{marginTop:'10vh', padding:'2rem', border:'0px solid #555',  color:'#fff', textAlign:'center', position:'relative',  }}>
 
 
 
